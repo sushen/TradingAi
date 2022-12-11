8402f93cd2cd30db4ca62fc8071212ffa710cf22
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-12-11 18:43:22 +0600

    Update: prediction.py * Remove warning * Added date time, make formatted and color

--- prediction.py
+++ prediction.py
@@ -1,26 +1,30 @@
 import time
-
+import warnings
+from datetime import datetime
 import pandas as pd
 import talib
 import joblib
+from sty import fg
 
+warnings.filterwarnings('ignore')  # warning will be ignored
 from dataframe import GetDataframe
+
 symbol = "BTCBUSD"
 
 while True:
     df = GetDataframe().get_minute_data(symbol, 1, 5)
-    df = df.iloc[:,0:10]
+    df = df.iloc[:, 0:10]
     df.astype(float)
     # df = df.drop(columns=['symbol','VolumeBUSD', 'CloseTime'])
     # df = df.iloc[0]
-    print(df)
+    # print(df)
     results = []
     cols = []
     for attr in dir(talib):
-        if attr[:3]=='CDL':
-    #         print(getattr(talib, attr))
+        if attr[:3] == 'CDL':
+            #         print(getattr(talib, attr))
             res = getattr(talib, attr)(df['Open'], df['High'], df['Low'],
-                                             df['Close'])
+                                       df['Close'])
             results.append(res)
             cols.append(attr)
     # print(results)
@@ -30,16 +34,18 @@ while True:
     patterns.columns = cols
     patterns.astype(float)
     patterns["Sum"] = patterns.sum(axis=1)
-    # patterns
-    print(patterns)
+    # print(patterns)
     df = df.add(patterns, fill_value=0)
     df = df.drop(['VolumeBUSD', 'CloseTime', 'Sum'], axis=1)
     df = df.iloc[-2]
-    print(df)
+    # print(df)
     # print(model.predict([df]))
-
 
     model = joblib.load("btcbusd_trand_predictor.joblib")
     predictions = model.predict([df])
-    print(predictions)
+
+    pred = fg.green + str(datetime.now()) + ' : ' + fg.rs + str(predictions)
+    print(pred)
     time.sleep(60)
+
+
