e78cc9efcc0aae837905e13b41088af5fc65d144
Sushen Biswas <sushenbiswasaga@gmail.com>
2023-05-07 11:22:55 +0600

    Update: prepares release

--- experiment/IP test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-server_time = "https://api.binance.com/api/v3/time"
-ip_query = 'https://checkip.amazonaws.com'
-
-response_1 = requests.request("GET", server_time)
-response_2 = requests.request("GET", ip_query)
-
-print(response_1.text,response_2.text)
--- main.py
+++ main.py
@@ -7,7 +7,14 @@ from indicator.indicators import CreateIndicators
 from get_symbol.find_symbols import FindSymbols
 import binance
 from database.exchange_info import BinanceExchange
-from network.network_status import BinanceNetwork
+from datetime import datetime
+from email_option.sending_mail import MailSender
+
+sender = MailSender()
+sender.login()
+
+from googlesheet.connection import Connection
+ws = Connection().connect_worksheet("tracker2")
 
 
 def main():
@@ -23,12 +30,6 @@ def main():
     # ticker_info = pd.DataFrame(APICall.client.get_ticker())
     # print(ticker_info)
 
-    bn = BinanceNetwork()
-    server_time = bn.get_server_time()
-    print("Server time: ", server_time)
-    time_diff = bn.get_time_diff()
-    print(f"The time difference between server and local machine is {time_diff:.2f} seconds")
-
     # fs = FindSymbols()
     # busd_symbole = fs.get_all_symbols("BUSD", ticker_info)
     # print(busd_symbole['symbol'])
@@ -41,6 +42,8 @@ def main():
 
     for symbol in all_symbols_payers:
         print(symbol)
+
+        # print(input("....:"))
 
         try:
             data = GetDataframe().get_minute_data(f'{symbol}', 1, 202)
@@ -60,45 +63,77 @@ def main():
 
         marker_sizes = np.abs(data['sum']) / 10
         # Add Buy and Sell signals
-        total_sum = 500
+        total_sum = 800
 
         # Plot is for conformation only Show when Signal is produced
         if not (any(data['sum'][-5:] >= total_sum) or any(data['sum'][-5:] <= -total_sum)):
             continue
 
         # Making Plot for batter visualization
-        plt.plot(data['Close'], label='Close Price')
-
+        # plt.plot(data['Close'], label='Close Price')
         buy_indices = data.index[data['sum'] >= total_sum]
         sell_indices = data.index[data['sum'] <= -total_sum]
-        plt.scatter(buy_indices, data['Close'][data['sum'] >= total_sum],
-                    marker='^', s=marker_sizes[data['sum'] >= total_sum], color='green', label='Buy signal', zorder=3)
-        plt.scatter(sell_indices, data['Close'][data['sum'] <= -total_sum],
-                    marker='v', s=marker_sizes[data['sum'] <= -total_sum], color='red', label='Sell signal', zorder=3)
+        # plt.scatter(buy_indices, data['Close'][data['sum'] >= total_sum],
+        #             marker='^', s=marker_sizes[data['sum'] >= total_sum], color='green', label='Buy signal', zorder=3)
+        # plt.scatter(sell_indices, data['Close'][data['sum'] <= -total_sum],
+        #             marker='v', s=marker_sizes[data['sum'] <= -total_sum], color='red', label='Sell signal', zorder=3)
+        # plt.get_current_fig_manager().set_window_title(f'{symbol} Signal')
 
-        plt.get_current_fig_manager().set_window_title(f'{symbol} Signal')
+        # Add Google sheet to see the signal later
+        # Email for get signal when we run it in the cloud
 
         # Add text labels for sum values
         for i, index in enumerate(buy_indices):
-            if df.index.get_loc(index) >= len(df)-5:
-                non_zero_cols = [col for col in df.columns if df.loc[index, col] != 0]
-                label = f"Sum: {data['sum'][index]}  Non-zero indicators: {', '.join(non_zero_cols)}"
-                print(label)
-            plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='bottom', fontsize=8)
-        for i, index in enumerate(sell_indices):
-            if df.index.get_loc(index) >= len(df)-5:
-                non_zero_cols = [col for col in df.columns if df.loc[index, col] != 0]
-                label = f"Sum: {data['sum'][index]}  Non-zero indicators: {', '.join(non_zero_cols)}"
-                print(label)
-            plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='top', fontsize=8)
+            if df.index.get_loc(index) >= len(df) - 5:
+                p_cols = [col + f"({str(df.loc[index, col])})" for col in df.columns if df.loc[index, col] != 0]
+                p = f"Sum: {data['sum'][index]}  Non-zero indicators: {', '.join(p_cols)}"
 
+                # Email Sending
+
+                print("The Bullish sound")
+                subject = symbol + " Bullish"
+                Body = f"Bullish signal for {symbol} symbol.\nTotal signal value: {data['sum'][index]}." \
+                       f"\n{p}."
+                sender.send_mail("zihad.bscincse@gmail.com", subject, Body)
+                sender.send_mail("tradingaitalib@gmail.com", subject, Body)
+
+                body = [str(datetime.now()), symbol, int(data['sum'][index]), p]
+                ws.append_row(body)
+
+                print(p)
+            # plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='bottom', fontsize=8)
+        for i, index in enumerate(sell_indices):
+            if df.index.get_loc(index) >= len(df) - 5:
+                p_cols = [col + f"({str(df.loc[index, col])})" for col in df.columns if df.loc[index, col] != 0]
+                p = f"Sum: {data['sum'][index]}  Non-zero indicators: {', '.join(p_cols)}"
+
+                # Email Sending
+
+                print("The Bearish sound")
+                subject = symbol + " Bearish"
+                Body = f"Bearish signal for {symbol} symbol.\nTotal signal value: {data['sum'][index]}." \
+                       f"\n{p}."
+                sender.send_mail("zihad.bscincse@gmail.com", subject, Body)
+                sender.send_mail("tradingaitalib@gmail.com", subject, Body)
+
+                body = [str(datetime.now()), symbol, int(data['sum'][index]), p]
+                ws.append_row(body)
+
+                print(p)
+            # plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='top', fontsize=8)
 
         # Instate of Figure write Symbol name
-        plt.title(symbol)
-        plt.legend()
-        plt.show()
-        time.sleep(4)
-        plt.close()
+        # plt.title(symbol)
+        # plt.legend()
+        # plt.show()
+        # time.sleep(4)
+        # plt.close()
 
 
-main()
+while True:
+    try:
+        main()
+    except:
+        time.sleep(61)
+        print("Error Happened Contact Developer")
+        main()
