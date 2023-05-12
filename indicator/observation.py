93300e1801cf193f5c52fd227f96a9f7ba603c17
Sushen Biswas <sushenbiswasaga@gmail.com>
2023-05-12 17:11:20 +0600

    Update: observation.py

--- indicator/observation.py
+++ indicator/observation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from indicator.indicators import CreateIndicators
 import binance
 from network.network_status import BinanceNetwork
+from database.resample import ResampleData
 
 # TODO :
 #  Reason: False Signal
@@ -26,13 +27,18 @@ def main():
     print(f"The time difference between server and local machine is {time_diff:.2f} seconds")
 
     total_sum = 800
-    symbol = "MATICBUSD"
+    symbol = "BTCBUSD"    #AGIXBUSD
     lookback = 1440
+    time = 3            # For 3 minute data
 
     try:
         data = GetDataframe().get_minute_data(f'{symbol}', 1, lookback)
     except binance.exceptions.BinanceAPIException as e:
         print(f"Binance API exception: {e}")
+
+    if time > 1:
+        rd = ResampleData(symbol)
+        data = rd.resample_to_minute(data, time)
 
     # print(data)
     ci = CreateIndicators(data)
@@ -61,29 +67,42 @@ def main():
     plt.scatter(sell_indices, data['Close'][data['sum'] <= -total_sum],
                 marker='v', s=marker_sizes[data['sum'] <= -total_sum], color='red', label='Sell signal', zorder=3)
 
-    plt.get_current_fig_manager().set_window_title(f'{symbol} Signal')
-
     # Add text labels for sum values
+    # for i, index in enumerate(buy_indices):
+    #     plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='bottom', fontsize=8)
+    # for i, index in enumerate(sell_indices):
+    #     plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='top', fontsize=8)
+
+    # Add text labels for sum values and non-zero columns
     for i, index in enumerate(buy_indices):
-        if df.index.get_loc(index) >= len(df) - 5:
-            non_zero_cols = [col for col in df.columns if df.loc[index, col] != 0]
-            # TODO: add cripto price
-            label = f"Sum: {data['sum'][index]}  Non-zero indicators: {', '.join(non_zero_cols)}"
-            print(label)
-        plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='bottom', fontsize=8)
+        non_zero_cols = [(col, int(df.loc[index, col])) for col in df.columns if
+                         df.loc[index, col] != 0]
+        non_zero_cols.sort(key=lambda x: x[1])
+        label = f"Sum: {data['sum'][index]}"
+        for j, (col, value) in enumerate(non_zero_cols):
+            # print(data['Close'][index])
+            y_offset = (j + 1) * 10 if value > 0 else -(j + 1) * 10
+            dot_color = 'g' if value > 0 else 'r'
+            plt.plot(index, data['Close'][index] + y_offset, f'{dot_color}o', markersize=2)
+            plt.text(index, data['Close'][index] + y_offset, f"{col}: {value}", color="#808080", ha='center', va='bottom', fontsize=8)
+        plt.text(index, data['Close'][index], label, color="#808080", ha='center', va='bottom', fontsize=8)
+
     for i, index in enumerate(sell_indices):
-        if df.index.get_loc(index) >= len(df) - 5:
-            non_zero_cols = [col for col in df.columns if df.loc[index, col] != 0]
-            label = f"Sum: {data['sum'][index]}  Non-zero indicators: {', '.join(non_zero_cols)}"
-            print(label)
-        plt.text(index, data['Close'][index], str(data['sum'][index]), ha='center', va='top', fontsize=8)
+        non_zero_cols = [(col, int(df.loc[index, col])) for col in df.columns if
+                         df.loc[index, col] != 0]
+        non_zero_cols.sort(key=lambda x: x[1])
+        label = f"Sum: {data['sum'][index]}"
+        for j, (col, value) in enumerate(non_zero_cols):
+            y_offset = (j + 1) * 10 if value > 0 else -(j + 1) * 10
+            dot_color = 'g' if value > 0 else 'r'
+            plt.plot(index, data['Close'][index] + y_offset, f'{dot_color}o', markersize=2)
+            plt.text(index, data['Close'][index] + y_offset, f"{col}: {value}", color="#808080", ha='center', va='top', fontsize=8)
+        plt.text(index, data['Close'][index], label, color="#808080", ha='center', va='top', fontsize=8)
 
     # Instate of Figure write Symbol name
     plt.title(symbol)
     plt.legend()
     plt.show()
-    time.sleep(4)
-    plt.close()
 
 
 main()
