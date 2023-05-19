e7efbe1f57ac62704842393878e1bb856d5a77a2
zihad <zihad.bscincse@gmail.com>
2023-05-19 09:09:24 +0600

    update: db_dataframe.py, missing_data.py "Did some improvement to improve the performance."

--- database/db_dataframe.py
+++ database/db_dataframe.py
@@ -4,11 +4,26 @@ from datetime import datetime
 
 class GetDbDataframe:
     def __init__(self, connection):
-        # Connect to the database
+        """
+        Initialize the GetDbDataframe object.
+
+        Parameters:
+        - connection: SQLite database connection object.
+        """
         self.connection = connection
 
-    def get_minute_data(self, symbol, time, limit):
-        # Execute the SQL query to fetch the last 250 records in ascending order
+    def get_minute_data(self, symbol, time, lookback):
+        """
+        Retrieve minute data for a symbol from the database.
+
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing the minute data.
+        """
         query = """
                     SELECT subquery.*
                     FROM (
@@ -17,9 +32,9 @@ class GetDbDataframe:
                         JOIN symbols ON asset_{time}m.symbol_id = symbols.id
                         WHERE symbols.symbolName = ?
                         ORDER BY asset_{time}m.id DESC
-                        LIMIT {limit}
+                        LIMIT {lookback}
                     ) AS subquery
-                    ORDER BY subquery.id ASC""".format(time=time, limit=limit//time)
+                    ORDER BY subquery.id ASC""".format(time=time, lookback=lookback)
 
         data = pd.read_sql_query(query, self.connection, params=(symbol, ))
         data = data.drop(['id', 'symbol_id'], axis=1)
@@ -29,7 +44,18 @@ class GetDbDataframe:
         data.rename(columns={f'Volume': f"Volume{symbol[:-4]}"}, inplace=True)
         return data
 
-    def get_crypto_candle(self, symbol, time, limit):
+    def get_crypto_candle(self, symbol, time, lookback):
+        """
+        Retrieve crypto candle data for a symbol from the database.
+
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing the crypto candle data.
+        """
         query = """
                     SELECT subquery.*
                     FROM (
@@ -38,14 +64,26 @@ class GetDbDataframe:
                         JOIN symbols ON cryptoCandle_{time}m.symbol_id = symbols.id
                         WHERE symbols.symbolName = ?
                         ORDER BY cryptoCandle_{time}m.id DESC
-                        LIMIT {limit}
+                        LIMIT {lookback}
                     ) AS subquery
-                    ORDER BY subquery.id ASC""".format(time=time, limit=limit // time)
-        data = pd.read_sql_query(query, self.connection, params=(symbol,))
+                    ORDER BY subquery.id ASC""".format(time=time, lookback=lookback)
+
+        data = pd.read_sql_query(query, self.connection, params=(symbol, ))
         data = data.drop(['id', 'symbol_id', 'asset_id'], axis=1)
         return data
 
-    def get_moving_average(self, symbol, time, limit):
+    def get_moving_average(self, symbol, time, lookback):
+        """
+        Retrieve moving average data for a symbol from the database.
+
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing the moving average data.
+        """
         query = """
                     SELECT subquery.*
                     FROM (
@@ -54,14 +92,26 @@ class GetDbDataframe:
                         JOIN symbols ON movingAverage_{time}m.symbol_id = symbols.id
                         WHERE symbols.symbolName = ?
                         ORDER BY movingAverage_{time}m.id DESC
-                        LIMIT {limit}
+                        LIMIT {lookback}
                     ) AS subquery
-                    ORDER BY subquery.id ASC""".format(time=time, limit=limit // time)
-        data = pd.read_sql_query(query, self.connection, params=(symbol,))
+                    ORDER BY subquery.id ASC""".format(time=time, lookback=lookback)
+
+        data = pd.read_sql_query(query, self.connection, params=(symbol, ))
         data = data.drop(['id', 'symbol_id', 'asset_id'], axis=1)
         return data
 
-    def get_macd(self, symbol, time, limit):
+    def get_macd(self, symbol, time, lookback):
+        """
+        Retrieve MACD data for a symbol from the database.
+
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing the MACD data.
+        """
         query = """
             SELECT subquery.*
             FROM (
@@ -70,15 +120,27 @@ class GetDbDataframe:
                 JOIN symbols ON macd_{time}m.symbol_id = symbols.id
                 WHERE symbols.symbolName = ?
                 ORDER BY macd_{time}m.id DESC
-                LIMIT {limit}
+                LIMIT {lookback}
             ) AS subquery
             ORDER BY subquery.id ASC
-        """.format(time=time, limit=limit // time)
+        """.format(time=time, lookback=lookback)
+
         data = pd.read_sql_query(query, self.connection, params=(symbol,))
         data = data.drop('id', axis=1)
         return data
 
-    def get_bollinger_bands(self, symbol, time, limit):
+    def get_bollinger_bands(self, symbol, time, lookback):
+        """
+        Retrieve Bollinger Bands data for a symbol from the database.
+
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing the Bollinger Bands data.
+        """
         query = """
             SELECT subquery.*
             FROM (
@@ -87,31 +149,55 @@ class GetDbDataframe:
                 JOIN symbols ON bollingerBands_{time}m.symbol_id = symbols.id
                 WHERE symbols.symbolName = ?
                 ORDER BY bollingerBands_{time}m.id DESC
-                LIMIT {limit}
+                LIMIT {lookback}
             ) AS subquery
             ORDER BY subquery.id ASC
-        """.format(time=time, limit=limit // time)
+        """.format(time=time, lookback=lookback)
+
         data = pd.read_sql_query(query, self.connection, params=(symbol,))
         data = data.drop('id', axis=1)
         return data
 
-    def get_super_trend(self, symbol, time, limit):
+    def get_super_trend(self, symbol, time, lookback):
+        """
+        Retrieve Super Trend data for a symbol from the database.
+
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing the Super Trend data.
+        """
         query = """
-                    SELECT subquery.*
-                    FROM (
-                        SELECT superTrend_{time}m.id, superTrend_{time}m.signal as super_trend
-                        FROM superTrend_{time}m
-                        JOIN symbols ON superTrend_{time}m.symbol_id = symbols.id
-                        WHERE symbols.symbolName = ?
-                        ORDER BY superTrend_{time}m.id DESC
-                        LIMIT {limit}
-                    ) AS subquery
-                    ORDER BY subquery.id ASC""".format(time=time, limit=limit // time)
+            SELECT subquery.*
+            FROM (
+                SELECT superTrend_{time}m.id, superTrend_{time}m.signal as super_trend
+                FROM superTrend_{time}m
+                JOIN symbols ON superTrend_{time}m.symbol_id = symbols.id
+                WHERE symbols.symbolName = ?
+                ORDER BY superTrend_{time}m.id DESC
+                LIMIT {lookback}
+            ) AS subquery
+            ORDER BY subquery.id ASC""".format(time=time, lookback=lookback)
+
         data = pd.read_sql_query(query, self.connection, params=(symbol,))
         data = data.drop('id', axis=1)
         return data
 
-    def get_rsi(self, symbol, time, limit):
+    def get_rsi(self, symbol, time, lookback):
+        """
+        Retrieve RSI data for a symbol from the database.
+
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing the RSI data.
+        """
         query = """
                     SELECT subquery.*
                     FROM (
@@ -120,31 +206,40 @@ class GetDbDataframe:
                         JOIN symbols ON rsi_{time}m.symbol_id = symbols.id
                         WHERE symbols.symbolName = ?
                         ORDER BY rsi_{time}m.id DESC
-                        LIMIT {limit}
+                        LIMIT {lookback}
                     ) AS subquery
-                    ORDER BY subquery.id ASC""".format(time=time, limit=limit // time)
+                    ORDER BY subquery.id ASC""".format(time=time, lookback=lookback)
+
         data = pd.read_sql_query(query, self.connection, params=(symbol,))
         data = data.drop('id', axis=1)
         return data
 
-    def get_all_indicators(self, symbol, time, limit):
+    def get_all_indicators(self, symbol, time, lookback):
+        """
+        Retrieve all indicators data for a symbol from the database.
 
-        df1 = self.get_crypto_candle(symbol, time, limit)
-        df2 = self.get_moving_average(symbol, time, limit)
-        df3 = self.get_macd(symbol, time, limit)
-        df4 = self.get_bollinger_bands(symbol, time, limit)
-        df5 = self.get_super_trend(symbol, time, limit)
-        df6 = self.get_rsi(symbol, time, limit)
+        Parameters:
+        - symbol: Symbol name.
+        - time: Time interval in minutes.
+        - lookback: Number of intervals to retrieve.
+
+        Returns:
+        - Pandas DataFrame containing all indicators data.
+        """
+        df1 = self.get_crypto_candle(symbol, time, lookback)
+        df2 = self.get_moving_average(symbol, time, lookback)
+        df3 = self.get_macd(symbol, time, lookback)
+        df4 = self.get_bollinger_bands(symbol, time, lookback)
+        df5 = self.get_super_trend(symbol, time, lookback)
+        df6 = self.get_rsi(symbol, time, lookback)
 
         data = pd.concat([df1, df2, df3, df4, df5, df6], axis=1)
-
         return data
-
 
 if __name__ == "__main__":
     connection = sqlite3.connect("big_crypto.db")
     db_frame = GetDbDataframe(connection)
-    data = db_frame.get_minute_data("BTCBUSD", 1, 30*1440)
+    data = db_frame.get_minute_data("BTCBUSD", 1, 30 * 1440)
     print(data)
-    indicator = db_frame.get_all_indicators("BTCBUSD", 1, 30*1440)
+    indicator = db_frame.get_all_indicators("BTCBUSD", 1, 30 * 1440)
     print(indicator)
